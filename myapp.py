abc1feb55a2e0dc67ccd886e34454968dedb7545
AchmadRaihan <91414460+AchmadRaihan@users.noreply.github.com>
2022-12-20 09:10:41 +0700

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -33,7 +33,3 @@ forecast = m.predict(future)
 st.write(f'Forecast plot for {n_years} days')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
-
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
